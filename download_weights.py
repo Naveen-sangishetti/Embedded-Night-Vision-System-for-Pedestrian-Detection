cd9d2617382b33f5b826f5c0d2d9fc01f7efb2d5
Naveen-sangishetti <sangishettinaveen04@gmail.com>
2026-05-28 00:50:07 +0530

    Fixed weight download link

--- download_weights.py
+++ download_weights.py
@@ -4,9 +4,10 @@ import urllib.request
 weights_path = "yolov2model/yolov2.weights"
 
 if not os.path.exists(weights_path):
+
     print("Downloading YOLOv2 weights...")
 
-    url = "https://pjreddie.com/media/files/yolov2.weights"
+    url = "https://github.com/pjreddie/darknet/raw/master/yolov2.weights"
 
     urllib.request.urlretrieve(url, weights_path)
 
